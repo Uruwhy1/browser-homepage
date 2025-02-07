72306482115eb097412da16547a656ffd78bd8b6
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-02-07 19:59:07 -0300

    add typescript

---
+++ typescript/settings.ts
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const openIcon = document.getElementById("settings-icon");
+
+  if (!openIcon) return;
+
+  openIcon.addEventListener("click", openSettingsTab);
+
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (e.code === "Escape") {
+      openSettingsTab();
+    }
+  });
+
+  initializeBookmarksSettings();
+  initializeSearchSettings();
+});
+
+function openSettingsTab() {
+  const settings = document.querySelector(".settings");
+
+  if (settings instanceof HTMLElement) {
+    settings.classList.toggle("show");
+  }
+}
+
+declare function initializeBookmarksSettings(): void;
+declare function initializeSearchSettings(): void;
